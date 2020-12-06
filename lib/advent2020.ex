baf4e1cdb8d610c156cdb061b8ac2c2b321d66a3
Mike Buhot <m.buhot@gmail.com>
2020-12-06 18:31:44 +1000

    Puzzle #3

--- lib/advent2020.ex
+++ lib/advent2020.ex
@@ -1,5 +1,4 @@
 defmodule Advent2020 do
-
   @doc """
   Successive tails of a list
 
@@ -41,12 +40,25 @@ defmodule Advent2020 do
   def combinations(_list, 0) do
     [[]]
   end
+
   def combinations([], _n) do
     []
   end
+
   def combinations([h | t], n) do
-    Enum.map(combinations(t, n-1), &[h | &1]) ++ combinations(t, n)
+    Enum.map(combinations(t, n - 1), &[h | &1]) ++ combinations(t, n)
   end
+
+  @doc """
+  The product of a list of numbers
+
+  ## Examples
+
+      iex> Advent2020.product([1,2,3,4,5])
+      1*2*3*4*5
+  """
+  @spec product(Enumerable.t()) :: number()
+  def product(numbers), do: Enum.reduce(numbers, &Kernel.*/2)
 
   defmodule Puzzle01 do
     @doc """
@@ -65,18 +77,18 @@ defmodule Advent2020 do
 
     ## Examples
 
-    iex> Advent2020.Puzzle01.find_product_where_sum([1,2,3,4], 2, 6)
+    iex> find_product_where_sum([1,2,3,4], 2, 6)
     2 * 4
 
-    iex> Advent2020.Puzzle01.find_product_where_sum([1,2,3,4,5], 3, 11)
+    iex> find_product_where_sum([1,2,3,4,5], 3, 11)
     2 * 4 * 5
     """
     @spec find_product_where_sum([integer], non_neg_integer, integer) :: integer()
     def find_product_where_sum(numbers, n, expected_sum) do
       numbers
       |> Advent2020.combinations(n)
-      |> Enum.find(&Enum.sum(&1) == expected_sum)
-      |> Enum.reduce(&Kernel.*/2)
+      |> Enum.find(&(Enum.sum(&1) == expected_sum))
+      |> Advent2020.product()
     end
 
     @spec part1 :: integer
@@ -92,11 +104,11 @@ defmodule Advent2020 do
 
   defmodule Puzzle02 do
     @type line :: %{
-      low: integer,
-      high: integer,
-      letter: binary,
-      password: binary
-    }
+            low: integer,
+            high: integer,
+            letter: binary,
+            password: binary
+          }
 
     @doc """
     Produces a stream of parsed lines from the input file
@@ -113,13 +125,20 @@ defmodule Advent2020 do
 
     ## Examples
 
-        iex> Advent2020.Puzzle02.parse_line("3-4 h: hrht")
+        iex> parse_line("3-4 h: hrht")
         %{low: 3, high: 4, letter: "h", password: "hrht"}
     """
     @spec parse_line(binary) :: line
     def parse_line(line) do
-      [low, high, letter, password] = Regex.run(~r/(\d+)-(\d+) (\w): (\w+)/, line, capture: :all_but_first)
-      %{low: String.to_integer(low), high: String.to_integer(high), letter: letter, password: password}
+      [low, high, letter, password] =
+        Regex.run(~r/(\d+)-(\d+) (\w): (\w+)/, line, capture: :all_but_first)
+
+      %{
+        low: String.to_integer(low),
+        high: String.to_integer(high),
+        letter: letter,
+        password: password
+      }
     end
 
     @doc """
@@ -127,18 +146,19 @@ defmodule Advent2020 do
 
     ## Examples
 
-        iex> Advent2020.Puzzle02.valid_password1?(%{low: 3, high: 4, letter: "h", password: "hrht"})
+        iex> valid_password1?(%{low: 3, high: 4, letter: "h", password: "hrht"})
         false
 
-        iex> Advent2020.Puzzle02.valid_password1?(%{low: 2, high: 5, letter: "h", password: "hrht"})
+        iex> valid_password1?(%{low: 2, high: 5, letter: "h", password: "hrht"})
         true
     """
     @spec valid_password1?(line) :: boolean
     def valid_password1?(%{low: l, high: h, letter: x, password: p}) do
-      Enum.member?(l .. h,
+      Enum.member?(
+        l..h,
         p
         |> String.graphemes()
-        |> Enum.count(& &1 == x)
+        |> Enum.count(&(&1 == x))
       )
     end
 
@@ -146,19 +166,19 @@ defmodule Advent2020 do
     Tests if a password policy line is valid (method 2)
 
     ## Examples
-        iex> Advent2020.Puzzle02.valid_password2?(%{low: 1, high: 3, letter: "a", password: "abcde"})
+        iex> valid_password2?(%{low: 1, high: 3, letter: "a", password: "abcde"})
         true
 
-        iex> Advent2020.Puzzle02.valid_password2?(%{low: 1, high: 3, letter: "b", password: "cdefg"})
+        iex> valid_password2?(%{low: 1, high: 3, letter: "b", password: "cdefg"})
         false
 
-        iex> Advent2020.Puzzle02.valid_password2?(%{low: 2, high: 9, letter: "c", password: "ccccccccc"})
+        iex> valid_password2?(%{low: 2, high: 9, letter: "c", password: "ccccccccc"})
         false
     """
     @spec valid_password2?(line) :: boolean
     def valid_password2?(%{low: l, high: h, letter: x, password: p}) do
       graphemes = String.graphemes(p)
-      [match1, match2] = Enum.map([l-1, h-1], &Enum.at(graphemes, &1) == x)
+      [match1, match2] = Enum.map([l - 1, h - 1], &(Enum.at(graphemes, &1) == x))
       :erlang.xor(match1, match2)
     end
 
@@ -172,6 +192,82 @@ defmodule Advent2020 do
     def part2 do
       data()
       |> Enum.count(&valid_password2?/1)
+    end
+  end
+
+  defmodule Puzzle03 do
+    def data do
+      "priv/puzzle03.txt"
+      |> File.stream!()
+      |> Stream.map(&String.trim/1)
+      |> Stream.filter(fn x -> x != "" end)
+    end
+
+    @spec sample :: [binary]
+    def sample do
+      """
+      ..##.......
+      #...#...#..
+      .#....#..#.
+      ..#.#...#.#
+      .#...##..#.
+      ..#.##.....
+      .#.#.#....#
+      .#........#
+      #.##...#...
+      #...##....#
+      .#..#...#.#
+      """
+      |> String.split()
+    end
+
+    @doc """
+    Count the trees following a 3-1 slope from the top left position
+
+    ## Examples
+
+        iex> count_trees(sample(), right: 1, down: 1)
+        2
+
+        iex> count_trees(sample(), right: 3, down: 1)
+        7
+
+        iex> count_trees(sample(), right: 5, down: 1)
+        3
+
+        iex> count_trees(sample(), right: 7, down: 1)
+        4
+
+        iex> count_trees(sample(), right: 1, down: 2)
+        2
+    """
+    @spec count_trees(Enumerable.t(), Keyword.t()) :: non_neg_integer()
+    def count_trees(data, right: r, down: d) do
+      data
+      |> Stream.take_every(d)
+      |> Stream.with_index()
+      |> Stream.map(fn {line, y} ->
+        length = byte_size(line)
+        x = rem(y * r, length)
+        binary_part(line, x, 1)
+      end)
+      |> Enum.count(& &1 == "#")
+    end
+
+    @spec part1 :: non_neg_integer
+    def part1 do
+      data() |> count_trees(right: 3, down: 1)
+    end
+
+    def part2 do
+      [
+        count_trees(data(), right: 1, down: 1),
+        count_trees(data(), right: 3, down: 1),
+        count_trees(data(), right: 5, down: 1),
+        count_trees(data(), right: 7, down: 1),
+        count_trees(data(), right: 1, down: 2)
+      ]
+      |> Advent2020.product()
     end
   end
 end
--- test/advent2020_test.exs
+++ test/advent2020_test.exs
@@ -5,27 +5,43 @@ defmodule Advent2020Test do
   doctest Advent2020
 
   describe "Puzzle #1" do
+    import Advent2020.Puzzle01
     doctest Advent2020.Puzzle01
 
     test "Part 1" do
-      assert Advent2020.Puzzle01.part1() == 388075
+      assert part1() == 388075
     end
 
     test "Part 2" do
-      assert Advent2020.Puzzle01.part2() == 293450526
+      assert part2() == 293450526
     end
   end
 
   describe "Puzzle #2" do
+    import Advent2020.Puzzle02
     doctest Advent2020.Puzzle02
 
     test "Part 1" do
-      assert Advent2020.Puzzle02.part1() == 474
+      assert part1() == 474
     end
 
     test "Part 2" do
-      assert Advent2020.Puzzle02.part2() == 745
+      assert part2() == 745
     end
 
+  end
+
+  describe "Puzzle #3" do
+    import Advent2020.Puzzle03
+
+    doctest Advent2020.Puzzle03
+
+    test "Part 1" do
+      assert part1() == 232
+    end
+
+    test "Part 2" do
+      assert part2() == 3952291680
+    end
   end
 end
